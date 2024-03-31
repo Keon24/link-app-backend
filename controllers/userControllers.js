56d46f4597fc805dc1590fc77f2ed1626eb1714d
Keon24 <keonpowell95@gmail.com>
2024-03-30 20:25:05 -0700

    login/controllers & extracted headers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/regitser", async (req, res) => {
     try {
     const {username,password} = req.body;
- const user = await UserModel.findOne(username)
+ const user = await UserModel.findOne({username})
  if(!user) {
     return res.status(400).json({type: usererrors.NO_USER_FOUND});
  }
@@ -22,4 +22,41 @@ router.post("/regitser", async (req, res) => {
    } catch (error) {
     res.status(500).json({type: err});        
     }
-}) 
+});
+
+router.post("/login", async (req,res) => {
+    try {
+        const {username,password} = req.body;
+        const user = await UserModel.findOne({username})
+      if(!user) {
+        return res.status(400).json({type: usererrors.NO_USER_FOUND});
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({type: usererrors.WRONG_CREDENTIALS});
+      }
+const token = jwt.sign ({id: user._id}, "secretwebapp");
+res.json({token, userID: user._id});
+
+    } catch (error) {
+        res.status.json(500).json({type: usererrors.WRONG_CREDENTIALS});
+    }
+});
+
+export const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      jwt.verify(authHeader, "secretwebapp", (err) => {
+        if (err) {
+          return res.sendStatus(403);
+        }
+        next();
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  };
+  
+  export { router as userRouter };
+  
+  
